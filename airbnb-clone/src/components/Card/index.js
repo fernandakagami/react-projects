97aeb8ed5dab45b212055a5bd9d69593754dda77
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-01-05 11:24:12 -0300

    refactoring

--- airbnb-clone/src/components/Card/index.js
+++ airbnb-clone/src/components/Card/index.js
@@ -6,15 +6,15 @@ export default function Card(props) {
   return (
     <section>
       <div className='card'>
-        <img src={props.img} alt='Katie Zaferes' className='card-image' />
+        <img src={props.img} alt={props.id} className='card-image' />
         <div className='card-stats'>
           <img src={star} alt='star' className='card-star' />
           <span>{props.rating}</span>
           <span className='gray'>({props.reviewCount}) • </span>
           <span className='gray'>{props.country}</span>
         </div>
-        <p>{props.title}</p>
-        <p><span className='bold'>From ${props.price}</span> / person</p>
+        <p className='card-title'>{props.title}</p>
+        <p className='card-price'><span className='bold'>From ${props.price}</span> / person</p>
       </div>
       
     </section>
