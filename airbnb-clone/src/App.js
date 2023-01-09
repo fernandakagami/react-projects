df898c81bb3c92e50ac817c88a877bf795f393f3
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-01-09 16:09:28 -0300

    changing props

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -8,7 +8,7 @@ function App() {
     return (
       <Card
         key={item.id}        
-        item={item}
+        {...item}
       />
     )
   })
--- airbnb-clone/src/components/Card/index.js
+++ airbnb-clone/src/components/Card/index.js
@@ -3,9 +3,9 @@ import star from '../../images/star.png'
 
 export default function Card(props) {  
   let badgeText
-  if (props.item.openSpots === 0) {
+  if (props.openSpots === 0) {
     badgeText = "SOLD OUT"
-  } else if (props.item.location === "Online") {
+  } else if (props.location === "Online") {
     badgeText = "ONLINE"
   }
 
@@ -13,15 +13,15 @@ export default function Card(props) {
     <section>
       <div className='card'>
         {badgeText && <div className='card-badge'>{badgeText}</div>}
-        <img src={require('../../images/' + props.item.coverImg)} alt={props.item.id} className='card-image' />
+        <img src={require('../../images/' + props.coverImg)} alt={props.id} className='card-image' />
         <div className='card-stats'>
           <img src={star} alt='star' className='card-star' />
-          <span>{props.item.stats.rating}</span>
-          <span className='gray'>({props.item.stats.reviewCount}) • </span>
-          <span className='gray'>{props.item.location}</span>
+          <span>{props.stats.rating}</span>
+          <span className='gray'>({props.stats.reviewCount}) • </span>
+          <span className='gray'>{props.location}</span>
         </div>
-        <p className='card-title'>{props.item.title}</p>
-        <p className='card-price'><span className='bold'>From ${props.item.price}</span> / person</p>
+        <p className='card-title'>{props.title}</p>
+        <p className='card-price'><span className='bold'>From ${props.price}</span> / person</p>
       </div>
       
     </section>
