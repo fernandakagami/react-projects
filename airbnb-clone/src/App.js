a8263c4af0c300dce2babc05343a2f157cffc06b
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-01-09 15:58:23 -0300

    refactoring

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -7,14 +7,8 @@ function App() {
   const cards = data.map(item => {
     return (
       <Card
-        key={item.id}
-        img={require('./images/' + item.coverImg)}
-        rating={item.stats.rating}
-        reviewCount={item.stats.reviewCount}
-        country={item.location}
-        title={item.title}
-        price={item.price}
-        openSpots={item.openSpots}
+        key={item.id}        
+        item={item}
       />
     )
   })
--- airbnb-clone/src/components/Card/index.js
+++ airbnb-clone/src/components/Card/index.js
@@ -1,21 +1,27 @@
 import './Card.css'
 import star from '../../images/star.png'
 
-
 export default function Card(props) {  
+  let badgeText
+  if (props.item.openSpots === 0) {
+    badgeText = "SOLD OUT"
+  } else if (props.item.location === "Online") {
+    badgeText = "ONLINE"
+  }
+
   return (
     <section>
       <div className='card'>
-        {props.openSpots === 0 && <div className='card-badge'>SOLD OUT</div>}
-        <img src={props.img} alt={props.id} className='card-image' />
+        {badgeText && <div className='card-badge'>{badgeText}</div>}
+        <img src={require('../../images/' + props.item.coverImg)} alt={props.item.id} className='card-image' />
         <div className='card-stats'>
           <img src={star} alt='star' className='card-star' />
-          <span>{props.rating}</span>
-          <span className='gray'>({props.reviewCount}) • </span>
-          <span className='gray'>{props.country}</span>
+          <span>{props.item.stats.rating}</span>
+          <span className='gray'>({props.item.stats.reviewCount}) • </span>
+          <span className='gray'>{props.item.location}</span>
         </div>
-        <p className='card-title'>{props.title}</p>
-        <p className='card-price'><span className='bold'>From ${props.price}</span> / person</p>
+        <p className='card-title'>{props.item.title}</p>
+        <p className='card-price'><span className='bold'>From ${props.item.price}</span> / person</p>
       </div>
       
     </section>
