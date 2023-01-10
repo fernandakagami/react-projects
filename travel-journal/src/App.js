9f85322a36382b2bcd9b0458260fac0e54a2efc4
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-01-10 10:19:38 -0300

    create project

---
+++ travel-journal/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Learn React       
+      </header>
+    </div>
+  );
+}
+
+export default App;
