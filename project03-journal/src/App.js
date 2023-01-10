cf82701d619b91139899c5478137231918376553
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-01-10 11:33:39 -0300

    Update App.js

--- project03-journal/src/App.js
+++ project03-journal/src/App.js
@@ -1,9 +1,23 @@
-import Header from "./components/Header";
+import Header from "./components/Header"
+import Item from "./components/Item"
+import data from "./data"
 
 function App() {
+  const items = data.map(item => {
+    return (
+      <Item
+        key={item.id}
+        {...item}
+      />
+    )
+  })
+
   return (
     <div>
       <Header />
+      <div>
+        {items}
+      </div>
     </div>
   );
 }
