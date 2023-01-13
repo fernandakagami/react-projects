b031679d96fda5f2139aa61c8490a9967c7b555f
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-01-13 19:29:26 -0300

    create new project

---
+++ project05-count/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React from "react";
+
+function App() {  
+  const [count, setCount] = React.useState(0)
+  
+  function add() {
+    setCount(prevCount => prevCount + 1)    
+  }
+  
+  function subtract() {
+    setCount(prevCount => prevCount - 1)
+  }
+  
+  return (
+    <div className="counter">
+        <button className="counter--minus" onClick={subtract}>–</button>
+        <div className="counter--count">
+            <h1>{count}</h1>
+        </div>
+        <button className="counter--plus" onClick={add}>+</button>
+    </div>
+  )  
+}
+
+export default App;
