9c23d26ed229ff5a61ff9a2a5c71c69da7e1d1e9
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-01-19 14:35:54 -0300

    use API

--- project04-meme-generator/src/components/Meme.js
+++ project04-meme-generator/src/components/Meme.js
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import memesData from '../memesData'
+import { useState, useEffect } from 'react'
 
 export default function Meme() {
     const [meme, setMeme] = useState({
@@ -7,13 +6,17 @@ export default function Meme() {
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
-    const [allMemeImages, setAllMemeImages] = useState(memesData)
+    const [allMemes, setAllMemes] = useState([])
     
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
     
     function getMemeImage() {
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
