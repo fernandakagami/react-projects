e4771a33ed90abc6f3ae474deca20e0adf68937b
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-01-19 11:03:45 -0300

    refactoring

--- project04-meme-generator/src/App.js
+++ project04-meme-generator/src/App.js
@@ -1,10 +1,9 @@
-import './App.css'
 import Header from './components/Header'
 import Meme from './components/Meme'
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
       <Meme />
     </div>
