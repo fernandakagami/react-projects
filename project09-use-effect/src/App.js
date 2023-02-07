08f06b0eb0f25731483630b0841944cf50aa68ae
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-02-07 14:55:42 -0300

    using useEffect and API

--- project09-use-effect/src/App.js
+++ project09-use-effect/src/App.js
@@ -1,13 +1,37 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [racas, setRacas] = useState([]);
+  const [busca, setBusca] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:8080/doguinhos')
+      .then(resposta => resposta.json())
+      .then(dados => {
+        setRacas(dados);
+      })
+  }, [])
+
+  useEffect(() => {
+    if (busca && busca.length > 3) {
+      fetch('http://localhost:8080/doguinhos?nome=' + busca)
+      .then(resposta => resposta.json())
+      .then(dados => {
+        setRacas(dados);
+      })
+    }    
+  }, [busca])
+
   return (
     <div className="App">
       <h1>Bem-vindo aos doguinhos!</h1>
       <h4>Confira abaixo uma lista de raças dos doguinhos.</h4>
+      <input placeholder='Buscar por raça' onChange={evento => setBusca(evento.target.value)} />
 
       <ul>
-        <li></li>
+        {racas.map(raca => <li key={raca.nome}>{raca.nome}</li>)}
       </ul>
     </div>
   );
