50ee97470c879c9760d807d89e3d2820834be045
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-02-28 21:33:39 -0300

    create new projects

---
+++ project15-alfood/src/componentes/ListaRestaurantes/index.tsx
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import IRestaurante from '../../interfaces/IRestaurante';
+import style from './ListaRestaurantes.module.scss';
+import Restaurante from './Restaurante';
+
+const ListaRestaurantes = () => {
+
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/v1/restaurantes/')
+      .then(resposta => {
+        setRestaurantes(resposta.data.results)
+      })
+      .catch(erro => {
+        console.log(erro)
+      })
+  }, [])
+
+  return (<section className={style.ListaRestaurantes}>
+    <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+    {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+  </section>)
+}
+
+export default ListaRestaurantes
